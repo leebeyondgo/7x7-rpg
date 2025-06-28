51dd8793590378ee38c93b1ad85e0aacad02a73b
leebeyondgo <asomegarden@gmail.com>
2025-06-28 22:17:59 +0900

    Add 7x7 board component with dpad

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders 7x7 board', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const board = screen.getByTestId('board');
+  expect(board.children).toHaveLength(49);
 });
